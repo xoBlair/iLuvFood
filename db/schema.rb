8cb93a10816cea2c29d459ceab5ac55d20e2a416
Akash Shekara <akashshekara1212@gmail.com>
2015-09-28 14:48:34 -0500

    removed image error

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 20150928191024) do
-
+ActiveRecord::Schema.define(version: 20150928191116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150928191024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "users_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|

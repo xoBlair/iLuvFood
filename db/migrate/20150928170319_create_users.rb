49ef357552dad5e1c51bb2ff38e030c4458fc5ef
Akash Shekara <akashshekara1212@gmail.com>
2015-09-28 12:16:59 -0500

    User table model

---
+++ db/migrate/20150928170319_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.text :first_name
+      t.text :last_name
+      t.text :email
+      t.text :password
+
+      t.timestamps null: false
+    end
+  end
+end

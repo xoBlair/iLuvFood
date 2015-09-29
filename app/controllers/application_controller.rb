b3200c4c7c6dadfd786205a33f4143f1d38b5870
Sean Haughey <sdhaughey@gmail.com>
2015-09-29 16:15:41 -0500

    sessions implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	  protect_from_forgery with: :exception
+	  
+	  include SessionsHelper
+
+	  before_action :set_current_user
+		
+	def set_current_user 
+		@current_user = User.find_by(id: session[:current_user_id])
+	end
 end

27d87f839b23e6b796282d4efc09b5a42e62c0be
Sean Haughey <sdhaughey@gmail.com>
2015-09-30 17:02:38 -0500

    debugging destroy function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,11 +56,9 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    # @user.destroy
-    @user = User.find(params[:id])
-
+    @user.destroy
     respond_to do |format|
-      # format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
       # format.html { redirect_to "/", notice: 'User was successfully destroyed.' }
 

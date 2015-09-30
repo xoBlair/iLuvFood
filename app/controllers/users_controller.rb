273f66c9942079d3fa638e867af0761ee9d538bd
Sean Haughey <sdhaughey@gmail.com>
2015-09-30 16:22:45 -0500

    testing destroy method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,8 +56,10 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      # format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
+      format.html { redirect_to "/", notice: 'User was successfully destroyed.' }
+
     end
   end
 

acee28195904447cbdccdb32294454fd8e592543
Sean Haughey <sdhaughey@gmail.com>
2015-10-01 17:47:41 -0500

    still debugging aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,11 +29,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
   config.paperclip_defaults = {
     :storage => :s3,
-    region: ENV['AWS_REGION'],
     :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 

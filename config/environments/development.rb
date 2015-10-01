4d81d8f4c7c4874e4a530ea42291d2d24f4ab4a5
Sean Haughey <sdhaughey@gmail.com>
2015-10-01 18:26:25 -0500

    still debugging aws

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,8 +12,8 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_region => 'us-west-1'
-    :region => ENV['AWS_REGION']
+    :s3_region => 'us-west-1',
+    :region => ENV['AWS_REGION'],
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],

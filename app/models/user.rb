cdf90f64117659958162e4c73fce79d0c057c0f7
Sean Haughey <sdhaughey@gmail.com>
2015-10-01 11:50:35 -0500

    fixed avatar sizing

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   def set_default_url
-  ActionController::Base.helpers.asset_path('default-avatar.png')
+  ActionController::Base.helpers.asset_path('default-avatar-resize.jpg')
 end
 
 end

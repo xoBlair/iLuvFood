1b75aff12ae5d2eeed807506f24de484b42cb36d
Blair Johnson <helloblairj@gmail.com>
2015-10-01 11:40:55 -0500

    added default recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,10 @@
 class Recipe < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: lambda { |avatar| avatar.instance.set_default_url}
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+    def set_default_url
+  ActionController::Base.helpers.asset_path('default-recipe.png')
+end
 end

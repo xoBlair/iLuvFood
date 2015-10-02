085b383db3484d1ce158b42f14a5c7b587a84a65
Akash Shekara <akashshekara1212@gmail.com>
2015-10-02 12:52:24 -0500

    Added search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
 	def create
-		if Tag.find_by(name: params[:search])
-			params[:@tags] = Tag.find_by(name: params[:search]) 
-			params[:@taggings] = Tagging.find_by(tag_id: params[:@tags]).recipe_id
-			@recipes = Recipe.find_by(id: params[:@taggings])
-		end
+		@tag = Tag.find_by(name: params[:search])
+		@result = @tag.recipes
 	end
-
 end
